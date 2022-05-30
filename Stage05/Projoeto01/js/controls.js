6a1208bf51a4907cda8e43a8501ea2dafee13763
Godoy <j.godoysoares@gmail.com>
2022-05-30 19:58:14 -0300

    projeto timer javascript

---
+++ Stage05/Projoeto01/js/controls.js
@@ -0,0 +1,9 @@
+function resetControls() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonStop.classList.add('hide')
+  buttonSet.classList.remove('hide')
+}
+
+// export default
+export default resetControls
--- Stage05/Projoeto01/js/index.js
+++ Stage05/Projoeto01/js/index.js
@@ -1,3 +1,10 @@
+//EcmaScript - ES6 Modules
+
+//default import
+import resetControls from './controls.js'
+
+// named import
+import { Timer } from './timer.js'
 //Event-driven = evento direcionado
 
 //progamação imperativa = dando ordens
@@ -14,24 +21,36 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut
+
+// ijeção de dependências
+const timer = Timer({
+  minutesDisplay,
+  secondsDisplay,
+  timerTimeOut,
+  resetControls
+})
 
 buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+  timer.countdown()
 })
 
 buttonPause.addEventListener('click', function () {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
+  clearTimeout(timerTimeOut)
 })
 
 buttonStop.addEventListener('click', function () {
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
-  buttonStop.classList.add('hide')
-  buttonSet.classList.remove('hide')
+  resetControls()
+  timer.resetTimer()
 })
 
 buttonSoundOff.addEventListener('click', function () {
@@ -42,4 +61,15 @@ buttonSoundOff.addEventListener('click', function () {
 buttonSoundOn.addEventListener('click', function () {
   buttonSoundOn.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function () {
+  let newMinutes = prompt('Quantos minutos?')
+
+  if (!newMinutes) {
+    timer.resetTimer()
+    return
+  }
+  minutes = newMinutes
+  updateTimerDisplay(minutes, 0)
 })
