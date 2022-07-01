e1fa3f5cb30a1e721dfa158e818971f167a5d505
Godoy <j.godoysoares@gmail.com>
2022-06-30 21:01:41 -0300

    desfazendo o modulo

--- desafioStage05/timer two/javascript/index.js
+++ desafioStage05/timer two/javascript/index.js
@@ -1,5 +1,3 @@
-import { press, finish, forest, rain, coffee, fire } from './sounds.js'
-
 // ----- VARIABLES -----
 
 const minutesDisplay = document.querySelector('.minutes')
@@ -192,7 +190,96 @@ function enableControls() {
   buttonSub.removeAttribute('disabled', 'disabled')
 }
 
-// ----- FUNCTIONS OF SOUND -----
+// ----- SOUND -----
+
+buttonSoundOn.addEventListener('click', function () {
+  buttonSoundOff.classList.remove('hide')
+  buttonSoundOn.classList.add('hide')
+  buttonForest.classList.remove('active')
+  buttonForest.classList.remove('activeDark')
+  buttonRain.classList.remove('active')
+  buttonRain.classList.remove('activeDark')
+  buttonStore.classList.remove('active')
+  buttonStore.classList.remove('activeDark')
+  buttonFire.classList.remove('active')
+  buttonFire.classList.remove('activeDark')
+  inputForest.classList.remove('active')
+  inputRain.classList.remove('active')
+  inputStore.classList.remove('active')
+  inputFire.classList.remove('active')
+  stopSound()
+})
+
+const forestSound = new Audio('./sounds/Floresta.wav')
+const storeSound = new Audio('./sounds/Cafeteria.wav')
+const rainSound = new Audio('./sounds/Chuva.wav')
+const fireSound = new Audio('./sounds/Lareira.wav')
+const buttonPress = new Audio('./sounds/button-press.wav')
+const kichenTimer = new Audio('./sounds/kichen-timer.mp3')
+
+forestSound.loop = true
+storeSound.loop = true
+rainSound.loop = true
+fireSound.loop = true
+
+function press() {
+  buttonPress.play()
+}
+
+function finish() {
+  kichenTimer.play()
+}
+
+function forest() {
+  forestSound.play()
+  storeSound.pause()
+  rainSound.pause()
+  fireSound.pause()
+}
+
+function rain() {
+  rainSound.play()
+  storeSound.pause()
+  forestSound.pause()
+  fireSound.pause()
+}
+
+function coffee() {
+  storeSound.play()
+  forestSound.pause()
+  rainSound.pause()
+  fireSound.pause()
+}
+
+function fire() {
+  fireSound.play()
+  storeSound.pause()
+  forestSound.pause()
+  rainSound.pause()
+}
+
+function stopSound() {
+  forestSound.pause()
+  rainSound.pause()
+  storeSound.pause()
+  fireSound.pause()
+}
+
+inputForest.addEventListener('change', function () {
+  forestSound.volume = this.value
+})
+
+inputRain.addEventListener('change', function () {
+  rainSound.volume = this.value
+})
+
+inputStore.addEventListener('change', function () {
+  storeSound.volume = this.value
+})
+
+inputFire.addEventListener('change', function () {
+  fireSound.volume = this.value
+})
 
 // ----- FUNCTIONS OF DARKMODE -----
 
